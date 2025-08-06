fd1e95f5188911c86940ee7b64a1b7b55be1104e
agubinskiy <alexanderg55@yandex.ru>
2025-08-06 12:12:51 +0300

    Переработан класс FilmService, исправлен баг с поиском по несуществующему режиссёру

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.jdbc.core.*;
+import ru.yandex.practicum.filmorate.dal.mappers.*;
+import ru.yandex.practicum.filmorate.model.*;
+
+import java.util.*;
+
+public interface DirectorStorage {
+    public List<Director> findAll();
+    public Optional<Director> findById(long id);
+    public boolean deleteFilmLike(long directorId);
+
+    public Director save(Director director);
+
+    public Director update(Director director);
+}
