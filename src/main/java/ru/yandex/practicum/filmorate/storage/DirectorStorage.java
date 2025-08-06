2fdf028b4c98735bc8d159e9b51d5136b5eb8aff
agubinskiy <alexanderg55@yandex.ru>
2025-08-06 17:11:54 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -8,7 +8,9 @@ import java.util.*;
 
 public interface DirectorStorage {
     public List<Director> findAll();
+
     public Optional<Director> findById(long id);
+
     public boolean deleteFilmLike(long directorId);
 
     public Director save(Director director);
